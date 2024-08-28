6b6313ffe62e6bb45d598edcc0de2f6c9074063c
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2024-08-28 10:52:53 +0100

    Normalise playlist Fixes #59

--- src/main/groovy/net/ebdon/spottozara/SpotToZara.groovy
+++ src/main/groovy/net/ebdon/spottozara/SpotToZara.groovy
@@ -5,7 +5,7 @@ import org.jaudiotagger.audio.AudioFileIO
 import org.jaudiotagger.audio.mp3.MP3AudioHeader
 import java.util.logging.Logger
 import java.util.logging.Level
-
+import net.ebdon.audio.Ffmpeg
 /**
  * Command Line Interface and playlist processing
  */
@@ -65,8 +65,23 @@ class SpotToZara {
   void run() {
     loadM3u8()
     fixMetadata()
+    normalise()
     saveAsZaraPlayList()
     saveAsM3uPlayList()
+  }
+
+  void normalise() {
+    log.info "Normalising tracks"
+    new Ffmpeg().normalise( tracks )
+  }
+
+  final List getTracks() {
+    m3u8.collect {idx,track ->
+      log.trace ">>$idx<${track.last()[-10..-1]}"
+      String trackPath = track.last().replaceAll('\\\\','/')
+        // work around for internal regex errors
+      trackPath.split('/').last()
+    }
   }
 
   void saveAsM3uPlayList() {
