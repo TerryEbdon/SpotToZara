f286370ba34d22227d8cf302bd54b2986d5c61f7
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2025-02-20 10:03:10 +0000

    Allow silence trimming to be disabled and configured

---
+++ src/main/resources/config.groovy
@@ -0,0 +1,13 @@
+/**
+@file
+@author Terry Ebdon
+@brief This file configures the app.
+*/
+
+silenceRemove {
+  startPeriods = 1
+  startSilence = 0.5
+  stoptSilence = 0.5
+  stopDuration = 1
+  enabled      = true
+}
